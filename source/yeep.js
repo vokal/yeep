c1644f835184fd779c744b2bdeaa731fae1d6ee0
Jarrett Widman <jarrett@24m2.com>
2014-11-24 15:35:51 -0600

    messing with percussion

--- source/yeep.js
+++ source/yeep.js
@@ -56,8 +56,58 @@
         yeep.tracks[ soundName ]();
     };
 
+    yeep.gains = {
+        "ar": function ( options )
+        {
+            var defaults = {
+                delaySec: 0,
+                attackSec: 0.05,
+                attackVol: 1,
+                releaseSec: 0.02
+            };
+
+            var set = extend ( {}, defaults, options );
+
+            var now = audioCtx.currentTime;
+
+            var gainNode = audioCtx.createGain();
+            gainNode.gain.setValueAtTime( 0, 0 );
+            gainNode.gain.setValueAtTime( 0.00001, now + set.delaySec );
+            gainNode.gain.exponentialRampToValueAtTime( set.attackVol, now + set.delaySec + set.attackSec );
+            gainNode.gain.exponentialRampToValueAtTime( 0.00001, now + set.delaySec + set.attackSec + set.releaseSec );
+
+            return ( gainNode );
+        },
+        "adsr": function ( options )
+        {
+            var defaults = {
+                delaySec: 0,
+                attackSec: 0.05,
+                attackVol: 1,
+                decaySec: 0.02,
+                decayVol: 0.8,
+                sustainSec: 0.2,
+                releaseSec: 0.02
+            };
+
+            var set = extend ( {}, defaults, options );
+
+            var now = audioCtx.currentTime;
+
+            var gainNode = audioCtx.createGain();
+            gainNode.gain.setValueAtTime( 0, 0 );
+            gainNode.gain.setValueAtTime( 0.00001, now + set.delaySec );
+            gainNode.gain.exponentialRampToValueAtTime( set.attackVol, now + set.delaySec + set.attackSec );
+            gainNode.gain.exponentialRampToValueAtTime( set.decayVol, now + set.delaySec + set.attackSec + set.decaySec );
+            gainNode.gain.exponentialRampToValueAtTime( set.decayVol, now + set.delaySec + set.attackSec + set.decaySec + set.sustainSec );
+            gainNode.gain.exponentialRampToValueAtTime( 0.00001, now + set.delaySec + set.attackSec + set.decaySec + set.sustainSec + set.releaseSec );
+
+            return ( gainNode );
+        }
+    };
+
     yeep.tones = {
-        "upDown": function ( options )
+        "ar": function ( options )
         {
             var defaults = {
                 oscType: "sine",
@@ -89,62 +139,60 @@
                 decaySec: 0.02,
                 decayVol: 0.8,
                 sustainSec: 0.2,
-                releaseSec: 0.02
+                releaseSec: 0.02,
+                preGainFilters: []
             };
 
             var set = extend ( {}, defaults, options );
 
             var osc = audioCtx.createOscillator();
             osc.type = set.oscType;
-            var gainNode = audioCtx.createGain();
-            osc.connect( gainNode );
-            gainNode.connect( audioCtx.destination );
+
+            var lastNode = osc;
+
+            set.preGainFilters.forEach( function ( filter )
+            {
+                lastNode.connect( filter );
+                lastNode = filter;
+            } );
+
             var now = audioCtx.currentTime;
 
             osc.frequency.setValueAtTime( set.freq, 0 );
 
-            gainNode.gain.setValueAtTime( 0, 0 );
-            gainNode.gain.setValueAtTime( 0.00001, now + set.delaySec );
-            gainNode.gain.exponentialRampToValueAtTime( set.attackVol, now + set.delaySec + set.attackSec );
-            gainNode.gain.exponentialRampToValueAtTime( set.decayVol, now + set.delaySec + set.attackSec + set.decaySec );
-            gainNode.gain.exponentialRampToValueAtTime( set.decayVol, now + set.delaySec + set.attackSec + set.decaySec + set.sustainSec );
-            gainNode.gain.exponentialRampToValueAtTime( 0.00001, now + set.delaySec + set.attackSec + set.decaySec + set.sustainSec + set.releaseSec );
+            var gainNode = yeep.gains.adsr( set );
 
+            lastNode.connect( gainNode );
+            gainNode.connect( audioCtx.destination );
             osc.start( 0 );
 
-            setTimeout( function () {
-                osc.stop();
-                osc = null;
-                gainNode = null;
-            }, 1000 * ( now + set.delaySec + set.attackSec + set.decaySec + set.sustainSec + set.releaseSec + 0.01 ) );
-
-            return ( osc );
+            return ( lastNode );
         }
     };
 
     yeep.tracks = {
         "ping": function ()
         {
-            yeep.tones.upDown( { freq: yeep.notes.C4 } );
+            yeep.tones.ar( { freq: yeep.notes.C4 } );
         },
         "add": function ()
         {
-            yeep.tones.upDown( { freq: yeep.notes.C4 } );
-            yeep.tones.upDown( { freq: yeep.notes.E4, delaySec: 0.2 } );
+            yeep.tones.ar( { freq: yeep.notes.C4 } );
+            yeep.tones.ar( { freq: yeep.notes.E4, delaySec: 0.2 } );
         },
         "remove": function ()
         {
-            yeep.tones.upDown( { freq: yeep.notes.E4 } );
-            yeep.tones.upDown( { freq: yeep.notes.C4, delaySec: 0.2 } );
+            yeep.tones.ar( { freq: yeep.notes.E4 } );
+            yeep.tones.ar( { freq: yeep.notes.C4, delaySec: 0.2 } );
         },
         "ring": function ()
         {
             for ( var i = 0; i < 2; i++ )
             {
-                yeep.tones.upDown( { freq: yeep.notes.C4, delaySec: 0.8 * i } );
-                yeep.tones.upDown( { freq: yeep.notes.C4, delaySec: 0.8 * i + 0.3 } );
-                yeep.tones.upDown( { freq: yeep.notes.G4, delaySec: 0.8 * i + 0.4 } );
-                yeep.tones.upDown( { freq: yeep.notes.E4, delaySec: 0.8 * i + 0.5 } );
+                yeep.tones.ar( { freq: yeep.notes.C4, delaySec: 0.8 * i } );
+                yeep.tones.ar( { freq: yeep.notes.C4, delaySec: 0.8 * i + 0.3 } );
+                yeep.tones.ar( { freq: yeep.notes.G4, delaySec: 0.8 * i + 0.4 } );
+                yeep.tones.ar( { freq: yeep.notes.E4, delaySec: 0.8 * i + 0.5 } );
             }
         },
         "sadTrombone": function ()
@@ -156,31 +204,53 @@
         },
         "snare": function ( options )
         {
-            var osc = yeep.tones.upDown( extend( {}, { oscType: "square", freq: yeep.notes[ "A3" ], upSec: 0.05, downSec: 0.5, vol: 1 }, options ) );
+            options = options || {};
+            var lp = audioCtx.createBiquadFilter();
+
+            lp.type = "lowpass";
+            lp.frequency.value = 100;
+            lp.frequency.setValueAtTime(100, audioCtx.currentTime);
+            lp.frequency.exponentialRampToValueAtTime(9000, audioCtx.currentTime + 0.1);
+
+            var noise = audioCtx.createWhiteNoise();
+            var noiseGain = yeep.gains.ar( { attackSec: 0, attackVol: 1, releaseSec: 1, delaySec: options.delaySec || 0 } );
+
+            noise.connect( noiseGain );
+            noiseGain.connect( lp );
+            lp.connect( audioCtx.destination );
         },
         "kick": function ( options )
         {
-            yeep.tones.upDown( extend( {}, { oscType: "sawtooth", freq: yeep.notes[ "A1" ], upSec: 0.05, downSec: 0.6, vol: 1 }, options ) );
+            var lp = audioCtx.createBiquadFilter();
+            lp.type = "lowpass";
+            lp.frequency.value = 500;
+            lp.frequency.setValueAtTime(500, audioCtx.currentTime);
+            lp.frequency.exponentialRampToValueAtTime(20, audioCtx.currentTime + 0.6);
+
+            yeep.tones.ar( extend( {}, { oscType: "square", freq: yeep.notes[ "A1" ], upSec: 0.02, downSec: 0.6, vol: 1, preGainFilters: [ lp ] }, options ) );
         },
         "splash": function ( options )
         {
-            var noise = audioCtx.createBrownNoise();
-            var noiseGain = audioCtx.createGain();
-            var noiseFilter = audioCtx.createBiquadFilter();
-            noiseGain.gain.value = 700;
-            noiseFilter.frequency.value = 5000;
-            noise.connect( noiseFilter );
-            noiseFilter.connect( noiseGain );
+            options = options || {};
+            var lp = audioCtx.createBiquadFilter();
 
-            var osc = yeep.tones.upDown( extend( {}, { oscType: "sawtooth", freq: yeep.notes[ "A6" ], upSec: 0.05, downSec: 0.8, vol: 1 }, options ) );
+            lp.type = "highpass";
+            lp.frequency.value = 2000;
+            lp.frequency.setValueAtTime(2000, audioCtx.currentTime);
+            lp.frequency.exponentialRampToValueAtTime(3000, audioCtx.currentTime + 0.5);
 
-            noiseGain.connect( osc.frequency );
+            var noise = audioCtx.createWhiteNoise();
+            var noiseGain = yeep.gains.ar( { attackSec: 0, attackVol: 1, releaseSec: 2, delaySec: options.delaySec || 0 } );
+
+            noise.connect( noiseGain );
+            noiseGain.connect( lp );
+            lp.connect( audioCtx.destination );
         },
         "rimshot": function ()
         {
             yeep.tracks.snare();
-            yeep.tracks.kick( { delaySec: 0.15 } );
-            yeep.tracks.splash( { delaySec: 0.5 } );
+            yeep.tracks.kick( { delaySec: 0.2 } );
+            yeep.tracks.splash( { delaySec: 0.65 } );
         }
     };
 
